9d4dc2ec59f2c8941f72d0be095a5aef96060338
Kumar Sunil <rasganiya.sunil98@gmail.com>
2023-08-12 18:19:09 +0530

    database added succesfully

---
+++ server/database/db.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db_username = process.env.DB_USERNAME;
+const db_password = process.env.DB_USERNAME;
+
+const Connection = () => {
+  const MONGODB_URI = `mongodb+srv://${db_username}:${db_password}@todo.manbenk.mongodb.net/?retryWrites=true&w=majority`;
+
+  mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+  mongoose.connection.on("connected", () => {
+    console.log("Database connected successfully.");
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Database disconnected.");
+  });
+
+  mongoose.connection.on("error", () => {
+    console.log("Error while connecting with the database.", error.message);
+  });
+};
+export default Connection;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import Connection from "./database/db.js";
 
 const PORT = 8000;
 const app = express();
+Connection();
 app.listen(PORT, () => {
   console.log(`The server is listening on http://localhost:${PORT}`);
 });
